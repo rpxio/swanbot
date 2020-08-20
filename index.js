fe73280d17ccf12f21a923335c43de86857c4ae1
Randall Pace <randall@rpx.io>
2020-08-19 23:31:19 -0600

    we workin

--- index.js
+++ index.js
@@ -1,20 +1,34 @@
-var args = process.argv.slice(2);
-const text = args[0].toUpperCase();
 const im = require('gm').subClass({ imageMagick: true });
+const Discord = require('discord.js');
+const fs = require('fs');
+const bot = new Discord.Client();
 
 const meme = im('meme.jpg');
+const memeoutput = './meme-out.jpg'
 
-meme
-  .font('./impact.ttf')
-  .fontSize(26)
-  .out('-background', 'none')
-  .out('-fill', 'black')
-  .out('-interline-spacing', '-2')
-  .out('-size', '300x', `caption: ${text}`)
-  .out('-geometry', '+80+310')
-  .out('-composite')
-  .write('meme-out.jpg', (err) => {
-    if (err) {
-      console.log(err);
-    }
-  });
+bot.on('message', message => {
+  var prefix = '!'
+  var msg = message.content;
+  var command = msg.split(' ');
+  var cmd = command[0];
+  var text = command.splice(1);
+  var text = text.join(' ');
+  var text = text.toUpperCase();
+
+  if (cmd === prefix + 'swan') {
+    meme
+      .font('./impact.ttf')
+      .out('-background', 'none')
+      .out('-fill', 'black')
+      .out('-interline-spacing', '-2')
+      .out('-size', '300x180', `caption: ${text}`)
+      .out('-geometry', '+80+310')
+      .out('-composite')
+      .write('meme-out.jpg', (err) => {
+        err ? console.log(err) : message.channel.send('', { files: [ "./meme-out.jpg" ]}).catch(err => console.log('something broke', err));
+      });
+  }
+
+});
+
+bot.login(TOKEN);
